06d739b5ae7909ff92a284bc110135b8dbf5d130
1juanherrera <juansebas751@gmail.com>
2023-08-21 16:22:50 -0500

    weather-app

--- src/App.jsx
+++ src/App.jsx
@@ -30,33 +30,35 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <form onSubmit={(e) => searchLocation(e)} className="search">
+    <div className='app'>
+      <form onSubmit={(e) => searchLocation(e)} className='search'>
         <input 
+        className='search-text'
         placeholder='Enter location'
-        type="text" />
+        type='text' />
+        <input type='submit' placeholder='search' onSubmit={(e) => searchLocation(e)} className='search-submit' />
       </form>
       <div className='container'>
         <div className='top'>
-          <div className="location">
+          <div className='location'>
             <p>{data.name}</p>
           </div>
-          <div className="temp">
+          <div className='temp'>
             {data.main ? <h1 className='degrees'>{((data.main?.temp - 32) * 5/9).toFixed()}°C</h1> : null}
             {data.main ? <h1 className='fahrenheit'>{data.main?.temp.toFixed()}°F</h1> : null}
           </div>
-          <div className="description">
+          <div className='description'>
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
         {
         data.name != undefined &&
         <div className='bottom'>
-        <div className="feels">
+        <div className='feels'>
           {data.main ?<p className='bold'>{data.main?.feels_like.toFixed()}°F</p> : null}
           <p>Feels Like</p>
         </div>
-        <div className="humidity">
+        <div className='humidity'>
           {data.main ? <p className='bold'>{data.main?.humidity}%</p> : null}
           <p>Humidity</p>
         </div>
